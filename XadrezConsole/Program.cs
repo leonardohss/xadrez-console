ed29c14d2ea27e3532ce79edf458edc9a2b30724
Leonardo Sousa <leorick97@gmail.com>
2019-07-15 22:51:18 -0300

    Created Posicao class

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace XadrezConsole
 {
@@ -6,7 +7,12 @@ namespace XadrezConsole
     {
         static void Main(string[] args)
         {
-            
+            Posicao p;
+            p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + p);
+
+            Console.ReadLine();
         }
     }
 }
